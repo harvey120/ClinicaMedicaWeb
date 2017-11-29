525a30139ee794690c17c6c8444deed17e8b4b8b
javam <javam@S10-96640.redes.local>
2017-11-29 11:07:58 -0600

    Dao´s agregados

--- src/java/com/ClinicaMedica/DAO/RevisionGeneralDao.java
+++ src/java/com/ClinicaMedica/DAO/RevisionGeneralDao.java
@@ -5,6 +5,7 @@
  */
 package com.ClinicaMedica.DAO;
 
+import com.ClinicaMedica.Modelo.RevisionGeneral;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
