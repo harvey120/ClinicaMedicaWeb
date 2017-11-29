6600d39fcb861841103372cc997abbe6d5a97331
javam <javam@S10-96632.redes.local>
2017-11-29 12:54:42 -0600

    Cambios mantenimiento

---
+++ src/java/Mantenimiento/dao_matenimiento.java
@@ -0,0 +1,358 @@
+package Mantenimiento;
+
+import com.ClinicaMedica.DAO.DAO;
+import com.ClinicaMedica.Modelo.Enfermedad;
+import com.ClinicaMedica.Modelo.Examen;
+import com.ClinicaMedica.Modelo.Medicamento;
+import com.ClinicaMedica.Modelo.Ocupacion;
+import com.ClinicaMedica.Modelo.Religion;
+import com.ClinicaMedica.Modelo.Turno;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class dao_matenimiento extends DAO {
+
+    private PreparedStatement pre;
+    String sql;
+    ResultSet rs;
+
+    /*-----------------------------------------insertar---------------------------------------------*/
+    public void insertarMedicamento() throws SQLException {
+        try {
+            this.Conectar();
+            sql = "INSERT INTO medicamento(idMedicamento, Nombre, Descripcion, FechaVencimiento, Proveedor_idProveedor, Inventario_idInventario)"
+                    + " VALUES (?,?,?,?,?,?)";
+            this.pre = this.getCn().prepareStatement(sql);
+            Medicamento med = new Medicamento();
+            pre.setInt(1, med.getIdmedicamento());
+            pre.setString(2, med.getNombre());
+            pre.setString(3, med.getDescripcion());
+            pre.setString(4, med.getFecha());
+            pre.setInt(5, med.getProvedor());
+            pre.setInt(6, med.getInventario());
+            pre.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println("no inserta medicamento: " + ex);
+        } finally {
+            this.Cerrar();
+        }
+    }
+
+    public void insertarOcupacion() throws SQLException {
+        try {
+            this.Conectar();
+            sql = "INSERT INTO ocupacion (IdOcupacion, Nombre) VALUES (?,?)";
+            this.pre = this.getCn().prepareStatement(sql);
+            Ocupacion ocu = new Ocupacion();
+            pre.setInt(1, ocu.getIdOcupacion());
+            pre.setString(2, ocu.getNombre());
+            pre.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println("no inserta ocupacion: " + ex);
+        } finally {
+            this.Cerrar();
+        }
+    }
+
+    public void insertarEnfermedad() throws SQLException {
+        try {
+            this.Conectar();
+            sql = "INSERT INTO enfermedad(IdEnfermedad, Nombre, CategoriaEnfermedad_IdCategoriaE) VALUES (?,?,?)";
+            this.pre = this.getCn().prepareStatement(sql);
+            Enfermedad enf = new Enfermedad();
+            pre.setInt(1, enf.getIdEnfermedad());
+            pre.setInt(2, enf.getIdCategoriaEnfermedad());
+            pre.setString(3, enf.getNombre());
+            pre.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println("no inserta enfermedad: " + ex);
+        } finally {
+            this.Cerrar();
+        }
+    }
+
+    public void insertarReligion() throws SQLException {
+        try {
+            this.Conectar();
+            sql = "INSERT INTO religion(IdReligion, Nombre) VALUES (?,?)";
+            this.pre = this.getCn().prepareStatement(sql);
+            Religion reli = new Religion();
+            pre.setInt(1, reli.getIdReligion());
+            pre.setString(2, reli.getNombre());
+            pre.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println("no inserta Religion: " + ex);
+        } finally {
+            this.Cerrar();
+        }
+    }
+
+    public void insertarExamen() throws SQLException {
+        try {
+            this.Conectar();
+            sql = "INSERT INTO `examen`(`IdExamen`, `Nombre`, `idCategoriaExamen`) VALUES (?,?,?))";
+            this.pre = this.getCn().prepareStatement(sql);
+            Examen ex = new Examen();
+            pre.setInt(1, ex.getIdExamen());
+            pre.setString(2, ex.getNombre());
+            pre.setInt(3, ex.getIdCategoriaExamen());
+            pre.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println("no inserta Examen: " + ex);
+        } finally {
+            this.Cerrar();
+        }
+    }
+
+    public void insertarTurno() throws SQLException {
+        try {
+            this.Conectar();
+            sql = "INSERT INTO turno(idTurno, HorarioEntrada, HorarioSalida) VALUES (?,?,?)";
+            this.pre = this.getCn().prepareStatement(sql);
+            Turno tur = new Turno();
+            pre.setInt(1, tur.getIdturno());
+            pre.setString(2, tur.getHorarioEntrada());
+            pre.setString(3, tur.getHorarioSalida());
+            pre.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println("no inserta turno: " + ex);
+        } finally {
+            this.Cerrar();
+        }
+    }
+
+    /*-----------------------------------------insertar---------------------------------------------*/
+
+ /*-----------------------------------------Listar---------------------------------------------*/
+ /*-----------------------------------------Listar---------------------------------------------*/
+ /*-----------------------------------------Modificar---------------------------------------------*/
+    public void modificarMedicamento() throws SQLException {
+        try {
+            this.Conectar();
+            sql = "UPDATE medicamento SET  Nombre=?, Descripcion=?,FechaVencimiento=?,"
+                    + "Proveedor_idProveedor=?,Inventario_idInventario=? WHERE idMedicamento=?";
+            this.pre = this.getCn().prepareStatement(sql);
+            Medicamento med = new Medicamento();
+            pre.setString(1, med.getNombre());
+            pre.setString(2, med.getDescripcion());
+            pre.setString(3, med.getFecha());
+            pre.setInt(4, med.getProvedor());
+            pre.setInt(5, med.getInventario());
+            pre.setInt(6, med.getIdmedicamento());
+            pre.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println("no modifico medicamento: " + ex);
+        } finally {
+            this.Cerrar();
+        }
+    }
+
+    public void modificarOcupacion() throws SQLException {
+        try {
+            this.Conectar();
+            sql = "UPDATE ocupacion SET Nombre=? WHERE IdOcupacion=?";
+            this.pre = this.getCn().prepareStatement(sql);
+            Ocupacion ocu = new Ocupacion();
+            pre.setString(1, ocu.getNombre());
+            pre.setInt(2, ocu.getIdOcupacion());
+            pre.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println("no inserta ocupacion: " + ex);
+        } finally {
+            this.Cerrar();
+        }
+    }
+
+    public void modificarEnfermedad() throws SQLException {
+        try {
+            this.Conectar();
+            sql = "UPDATE enfermedad SET Nombre=?,CategoriaEnfermedad_IdCategoriaE=? WHERE IdEnfermedad=?";
+            this.pre = this.getCn().prepareStatement(sql);
+            Enfermedad enf = new Enfermedad();
+            pre.setInt(1, enf.getIdCategoriaEnfermedad());
+            pre.setString(2, enf.getNombre());
+            pre.setInt(3, enf.getIdEnfermedad());
+            pre.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println("no inserta enfermedad: " + ex);
+        } finally {
+            this.Cerrar();
+        }
+    }
+
+    public void modificarReligion() throws SQLException {
+        try {
+            this.Conectar();
+            sql = "UPDATE religion SET Nombre = ? WHERE IdReligion = ? ";
+            this.pre = this.getCn().prepareStatement(sql);
+            Religion reli = new Religion();
+            pre.setString(1, reli.getNombre());
+            pre.setInt(2, reli.getIdReligion());
+            pre.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println("no inserta Religion: " + ex);
+        } finally {
+            this.Cerrar();
+        }
+    }
+
+    public void modificarExamen() throws SQLException {
+        try {
+            this.Conectar();
+            sql = "UPDATE examen SET Nombre=?,idCategoriaExamen=? WHERE IdExamen=?";
+            this.pre = this.getCn().prepareStatement(sql);
+            Examen ex = new Examen();
+            pre.setString(1, ex.getNombre());
+            pre.setInt(2, ex.getIdCategoriaExamen());
+            pre.setInt(3, ex.getIdExamen());
+            pre.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println("no inserta Examen: " + ex);
+        } finally {
+            this.Cerrar();
+        }
+    }
+
+    public void modificarTurno() throws SQLException {
+        try {
+            this.Conectar();
+            sql = "UPDATE turno SET HorarioEntrada=?, HorarioSalida=? WHERE idTurno=?";
+            this.pre = this.getCn().prepareStatement(sql);
+            Turno tur = new Turno();
+            pre.setString(1, tur.getHorarioEntrada());
+            pre.setString(2, tur.getHorarioSalida());
+            pre.setInt(3, tur.getIdturno());
+            pre.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println("no inserta ocupacion: " + ex);
+        } finally {
+            this.Cerrar();
+        }
+    }
+
+    /*-----------------------------------------Modificar---------------------------------------------*/
+
+ /*-----------------------------------------Buscar---------------------------------------------*/
+    public Medicamento buscarMedicamento(Medicamento m) throws SQLException {
+
+        Medicamento men = new Medicamento();
+        try {
+            this.Conectar();
+            sql = "SELECT * FROM medicamento WHERE idMedicamento=?";
+            this.pre = this.getCn().prepareStatement(sql);
+            pre.setInt(1, m.getIdmedicamento());
+            rs = pre.executeQuery();
+            while (rs.next()) {
+                men.setNombre(rs.getString("Nombre"));
+                men.setDescripcion(rs.getString("Descripcion"));
+                men.setFecha(rs.getString("FechaVencimiento"));
+                men.setProvedor(rs.getInt("Proveedor_idProveedor"));
+                men.setInventario(rs.getInt("Inventario_idInventario"));
+                men.setIdmedicamento(rs.getInt("idMedicamento"));
+            }
+        } catch (SQLException ex) {
+            System.out.println("no inserta ocupacion: " + ex);
+        } finally {
+            this.Cerrar();
+        }
+        return men;
+    }
+
+    public Ocupacion buscarOcupacion(Ocupacion o) throws SQLException {
+        Ocupacion ocu = new Ocupacion();
+        try {
+            this.Conectar();
+            sql = "SELECT * FROM ocupacion WHERE IdOcupacion=?";
+            this.pre = this.getCn().prepareStatement(sql);
+            pre.setInt(1, o.getIdOcupacion());
+            rs = pre.executeQuery();
+            while (rs.next()) {                
+                ocu.setIdOcupacion(rs.getInt("IdOcupacion"));
+                ocu.setNombre(rs.getString("Nombre"));
+            }
+        } catch (SQLException ex) {
+            System.out.println("no inserta ocupacion: " + ex);
+        } finally {
+            this.Cerrar();
+        }
+        return ocu;
+    }
+
+    public Enfermedad buscarEnfermedad(Enfermedad e) throws SQLException {
+        Enfermedad enf = new Enfermedad();
+        try {
+            this.Conectar();
+            sql = "SELECT * FROM enfermedad WHERE IdEnfermedad=?";
+            this.pre = this.getCn().prepareStatement(sql);
+            pre.setInt(1, e.getIdEnfermedad());
+            rs = pre.executeQuery();
+            while (rs.next()) {                
+                enf.setIdEnfermedad(rs.getInt("IdEnfermedad"));
+                enf.setNombre(rs.getString("Nombre"));
+                enf.setIdCategoriaEnfermedad(rs.getInt("CategoriaEnfermedad_IdCategoriaE"));
+            }
+        } catch (SQLException ex) {
+            System.out.println("no inserta enfermedad: " + ex);
+        } finally {
+            this.Cerrar();
+        }
+        return enf;
+    }
+
+    public Religion buscarReligion(Religion r) throws SQLException {
+        Religion reli = new Religion();
+        try {
+            this.Conectar();
+            sql = "SELECT * FROM religion WHERE IdReligion=?";
+            this.pre = this.getCn().prepareStatement(sql); 
+            pre.setInt(1, r.getIdReligion());
+            rs=pre.executeQuery();
+            while (rs.next()) {                
+                reli.setIdReligion(rs.getInt("IdReligion"));
+                reli.setNombre(rs.getString("Nombre"));
+            }
+        } catch (SQLException ex) {
+            System.out.println("no inserta Religion: " + ex);
+        } finally {
+            this.Cerrar();
+        }return reli;
+    }
+
+    public Examen buscarExamen(Examen e) throws SQLException {
+        Examen exe = new Examen();
+        try {
+            this.Conectar();
+            sql = "INSERT INTO `examen`(`IdExamen`, `Nombre`, `idCategoriaExamen`) VALUES (?,?,?))";
+            this.pre = this.getCn().prepareStatement(sql);    
+            pre.setInt(1, e.getIdExamen());
+            rs=pre.executeQuery();
+            while (rs.next()) {                
+                
+            }
+        } catch (SQLException ex) {
+            System.out.println("no inserta Examen: " + ex);
+        } finally {
+            this.Cerrar();
+        }return exe;            
+    }
+
+    public String buscarTurno() throws SQLException {
+        try {
+            this.Conectar();
+            sql = "INSERT INTO turno(idTurno, HorarioEntrada, HorarioSalida) VALUES (?,?,?)";
+            this.pre = this.getCn().prepareStatement(sql);
+            Turno tur = new Turno();
+            pre.setInt(1, tur.getIdturno());
+            pre.setString(2, tur.getHorarioEntrada());
+            pre.setString(3, tur.getHorarioSalida());
+            pre.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println("no inserta ocupacion: " + ex);
+        } finally {
+            this.Cerrar();
+        }
+        return null;
+    }
+    /*-----------------------------------------Buscar---------------------------------------------*/
+}
---
+++ src/java/com/ClinicaMedica/Modelo/Medicamento.java
@@ -0,0 +1,60 @@
+package com.ClinicaMedica.Modelo;
+
+public class Medicamento {
+    private int idmedicamento;
+    private String nombre;
+    private String descripcion;
+    private String fecha;
+    private int provedor;
+    private int inventario;
+
+    public int getIdmedicamento() {
+        return idmedicamento;
+    }
+
+    public void setIdmedicamento(int idmedicamento) {
+        this.idmedicamento = idmedicamento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getProvedor() {
+        return provedor;
+    }
+
+    public void setProvedor(int provedor) {
+        this.provedor = provedor;
+    }
+
+    public int getInventario() {
+        return inventario;
+    }
+
+    public void setInventario(int inventario) {
+        this.inventario = inventario;
+    }
+    
+    
+}
