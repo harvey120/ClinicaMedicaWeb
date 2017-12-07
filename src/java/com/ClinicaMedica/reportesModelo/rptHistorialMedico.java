091d93856016748beb541d1ad34ac0b4b39077e2
javam <javam@S10-96641.redes.local>
2017-12-07 13:01:22 -0600

    actualizacion casi finalizada ADMIN

---
+++ src/java/com/ClinicaMedica/reportesModelo/rptHistorialMedico.java
@@ -0,0 +1,40 @@
+package com.ClinicaMedica.reportesModelo;
+
+/**
+ * @author harvey
+ */
+public class rptHistorialMedico {
+    
+    private String nombrePaciente;
+    private String fechaNacimiento;
+    private String telefono;
+    private String correo;
+    private String sexo;
+    private String civil;
+    private String documento;
+    private String noDocumento;
+    private String religion;
+    private String direccion;
+    private String detalleVivienda;
+    private String personaEncargada;
+//    private String 
+//    private String
+//    private String
+//    private String
+//    private String
+//    private String
+//    private String
+//    private String
+//    private String
+//    private String
+//    private String
+//    private String
+//    private String
+//    private String
+//    private String
+//    private String
+//    private String
+//    private String
+//    private String
+//    
+}
