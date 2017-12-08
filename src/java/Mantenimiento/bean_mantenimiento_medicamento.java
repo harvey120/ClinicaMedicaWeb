0ad7895edbe75dec058ff8977f7fd6604cb691a7
javam <javam@S10-96632.redes.local>
2017-12-08 12:49:26 -0600

    Cambios mantenimiento

--- src/java/Mantenimiento/bean_mantenimiento_medicamento.java
+++ src/java/Mantenimiento/bean_mantenimiento_medicamento.java
@@ -9,10 +9,11 @@ import javax.faces.bean.ViewScoped;
 @ManagedBean
 @ViewScoped
 public class bean_mantenimiento_medicamento {
-    Provedor pro = new Provedor();
-    Medicamento men = new Medicamento();
-    ArrayList<Medicamento> lista= new ArrayList();
-    ArrayList<Provedor> listo= new ArrayList();
+
+  Provedor pro = new Provedor();
+  Medicamento men = new Medicamento();
+  ArrayList<Medicamento> lista = new ArrayList();
+  ArrayList<Provedor> listo = new ArrayList();
 
   public Provedor getPro() {
     return pro;
@@ -30,104 +31,111 @@ public class bean_mantenimiento_medicamento {
     this.listo = listo;
   }
 
-    
-    
-    public Medicamento getMen() {
-        return men;
+  public Medicamento getMen() {
+    return men;
+  }
+
+  public void setMen(Medicamento men) {
+    this.men = men;
+  }
+
+  public ArrayList<Medicamento> getLista() {
+    return lista;
+  }
+
+  public void setLista(ArrayList<Medicamento> lista) {
+    this.lista = lista;
+  }
+
+  public void insertar() {
+    dao_matenimiento dao;
+    System.out.println("ha ingresado a insertar bn");
+
+    try {
+      dao = new dao_matenimiento();
+      dao.insertarMedicamento(men);
+      this.listar();
+    } catch (Exception e) {
+      System.out.println("error de insertar bn");
+    }
+    System.out.println("ha ingresado a insertar examen bn");
+
+  }
+
+  public void listar() {
+    System.out.println("ha ingresado a lista bn");
+    dao_matenimiento dao;
+
+    try {
+      dao = new dao_matenimiento();
+      lista = dao.listarMedicamento();
+      
+    } catch (Exception e) {
+      System.out.println("error listar bn" + e);
+    }
+  }
+
+  public void modificar() {
+    dao_matenimiento dao;
+    System.out.println("ha ingresado a modificar bn");
+    try {
+      
+      dao = new dao_matenimiento();
+      dao.modificarMedicamento(men);
+      this.listar();
+    } catch (Exception e) {
+      System.out.println("error de modificar bn" + e);
     }
 
-    public void setMen(Medicamento men) {
-        this.men = men;
+  }
+
+  public void buscar() {
+    System.out.println("ha ingresado a lista bn");
+    dao_matenimiento dao = null;
+    try {
+      
+        dao = new dao_matenimiento();
+        men = dao.buscarMedicamento(men);
+
+
+    } catch (Exception e) {
+
+      System.out.println("error listar bn" + e);
     }
+  }
+  
+  
+  
 
-    public ArrayList<Medicamento> getLista() {
-        return lista;
-    }
-
-    public void setLista(ArrayList<Medicamento> lista) {
-        this.lista = lista;
-    }
-    
-     public void insertar() {
-        dao_matenimiento dao;
-        System.out.println("ha ingresado a insertar bn");
-
-        try {
-            dao = new dao_matenimiento();
-            dao.insertarMedicamento(men);
-            this.listar();
-        } catch (Exception e) {
-            System.out.println("error de insertar bn");
-        }
-        System.out.println("ha ingresado a insertar examen bn");
-
-    }
-
-    public void listar() {
-        System.out.println("ha ingresado a lista bn");
-        dao_matenimiento dao;
-
-        try {
-            dao = new dao_matenimiento();
-            lista = dao.listarMedicamento();
-            for (int i = 0; i < lista.size(); i++) {
-                System.out.println("Codigo: " + lista.get(i).getNombre());
+  public void buscar2(Medicamento men) {
+    System.out.println("ha ingresado a lista bn");
+    dao_matenimiento dao;
+    Medicamento temp;
+    try {
+      dao = new dao_matenimiento();
+      temp = dao.LeerID(men);
+      if (temp != null) {
+                this.men = temp;
             }
-        } catch (Exception e) {
-            System.out.println("error listar bn" + e);
-        }
-    }
+    } catch (Exception e) {
 
-    public void modificar() {
-        dao_matenimiento dao;
-        System.out.println("ha ingresado a modificar bn");
-        try {
-
-            dao = new dao_matenimiento();
-            dao.modificarMedicamento(men);
-            this.listar();
-        } catch (Exception e) {
-            System.out.println("error de modificar bn" + e);
-        }
-        
+      System.out.println("error listar bn" + e);
     }
-    
-    public void buscar() {
-        System.out.println("ha ingresado a lista bn");
-        dao_matenimiento dao;
-        try {
-            dao = new dao_matenimiento();
-            men=dao.buscarMedicamento(men);
-            
-        } catch (Exception e) {
-            
-            System.out.println("error listar bn" + e);
-        }
-     }
-    
-    public void buscar2(Medicamento me) {
-        System.out.println("ha ingresado a lista bn");
-        dao_matenimiento dao;
-        try {
-            dao = new dao_matenimiento();
-            men=dao.buscarMedicamento(me);
-            
-        } catch (Exception e) {
-            
-            System.out.println("error listar bn" + e);
-        }
-     }
-    
-    public void listarnuevo() {
-        System.out.println("ha ingresado a lista bn");
-        dao_listas da;
+  }
+  
+  
+  
 
-        try {
-            da = new dao_listas();
-            listo = da.listarunico();
-            
-        } catch (Exception e) {
-            System.out.println("error listar bn" + e);
-        }
+  public void listarnuevo() {
+    System.out.println("ha ingresado a lista bn");
+    dao_listas da;
+
+    try {
+      da = new dao_listas();
+      listo = da.listarunico();
+
+    } catch (Exception e) {
+      System.out.println("error listar bn" + e);
     }
+  }
 }
